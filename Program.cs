8fdc5d7f5549d88cd58ef334211c66c94c6a467a
JavontaeH <hardeman.javontae@gmail.com>
2022-05-26 13:16:30 -0500

    finished phase 1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PlanYourHeist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist!");
+            TeamMember phase1 = new TeamMember();
+            Console.WriteLine("Enter You Team member's name");
+            phase1.Name = Console.ReadLine();
+            Console.WriteLine("Enter You Team member's skill level");
+            phase1.SkillLevel = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter You Team member's courage factor");
+            phase1.CourageFactor = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"Name: {phase1.Name} SkillLevel: {phase1.SkillLevel} CourageFactor: {phase1.CourageFactor}");
+        }
+    }
+}
---
+++ TeamMember.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PlanYourHeist
+{
+
+    public class TeamMember
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public double CourageFactor { get; set; }
+    }
+}
