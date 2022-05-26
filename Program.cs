133b753a1084f15f33ec937b679c5c65201ebc98
JavontaeH <hardeman.javontae@gmail.com>
2022-05-26 14:26:01 -0500

    finished phase 5

--- Program.cs
+++ Program.cs
@@ -8,8 +8,7 @@ namespace PlanYourHeist
         static void Main(string[] args)
         {
             Random rand = new Random();
-            int luck = rand.Next(-10, 11);
-            int bankDifficulty = 100 + luck;
+
             Crew myCrew = new Crew();
             Console.WriteLine("Plan Your Heist!");
             Console.WriteLine("Enter your team members name");
@@ -31,21 +30,28 @@ namespace PlanYourHeist
                 }
                 else
                 {
+                    Console.WriteLine("Enter your number of trial runs");
+                    int trialRuns = int.Parse(Console.ReadLine());
                     Console.WriteLine($"Your crew has {myCrew.TeamMembers.Count} team members!");
-                    for (int i = 0; i < myCrew.TeamMembers.Count; i++)
-                    {
-                        var crewMember = myCrew.TeamMembers.ElementAt(i).Value;
-
-                    }
                     Console.WriteLine($"The team's combined skill level is: {myCrew.sumOfSkill}");
-                    Console.WriteLine($"The bank's difficulty level is: {bankDifficulty}");
-                    if (myCrew.sumOfSkill >= bankDifficulty)
+                    for (int i = 0; i < trialRuns; i++)
                     {
-                        Console.WriteLine("Success!");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Failure!");
+                        int luck = rand.Next(-10, 11);
+                        int bankDifficulty = 100 + luck;
+                        for (int ii = 0; ii < myCrew.TeamMembers.Count; ii++)
+                        {
+                            var crewMember = myCrew.TeamMembers.ElementAt(ii).Value;
+
+                        }
+                        Console.WriteLine($"The bank's difficulty level is: {bankDifficulty}");
+                        if (myCrew.sumOfSkill >= bankDifficulty)
+                        {
+                            Console.WriteLine("Success!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Failure!");
+                        }
                     }
                 }
             }
